d027c8fbb3ff5db5b08539cfae8dbfd473a1e967
sean <sdoody95@gmail.com>
2022-03-24 15:05:37 -0400

    code formatting aligned

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,15 @@ import { AppComponent } from './app.component';
 import { ToDoComponent } from './components/to-do/to-do.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ToDoComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ToDoComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/to-do/to-do.component.ts
+++ src/app/components/to-do/to-do.component.ts
@@ -2,54 +2,54 @@ import { Component, OnInit, ɵisBoundToModule__POST_R3__ } from '@angular/core';
 import { ToDo } from 'src/app/models/to-do';
 
 @Component({
-  selector: 'app-to-do',
-  templateUrl: './to-do.component.html',
-  styleUrls: ['./to-do.component.css']
+    selector: 'app-to-do',
+    templateUrl: './to-do.component.html',
+    styleUrls: ['./to-do.component.css']
 })
 export class ToDoComponent implements OnInit {
 
-  toDos: ToDo[] = [
-    { task: 'rake and bag leaves', completed: false },
-    { task: 'clean bedroom', completed: true },
-    { task: 'do laundry', completed: true },
-    { task: 'finish Angular project', completed: false },
-    { task: 'take down Halloween decorations', completed: false },
-    { task: 'cook dinner', completed: true },
-  ];
+    toDos: ToDo[] = [
+        { task: 'rake and bag leaves', completed: false },
+        { task: 'clean bedroom', completed: true },
+        { task: 'do laundry', completed: true },
+        { task: 'finish Angular project', completed: false },
+        { task: 'take down Halloween decorations', completed: false },
+        { task: 'cook dinner', completed: true },
+    ];
 
-  filteredToDos: ToDo[] = [];
+    filteredToDos: ToDo[] = [];
 
-  filterInput: string = '';
-  toDoInput: string = '';
+    filterInput: string = '';
+    toDoInput: string = '';
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-    this.filteredToDos = this.toDos;
-  }
-
-  addTask(): void {
-    if (this.toDoInput !== '') {
-      this.toDos.push({ task: this.toDoInput, completed: false });
-      this.toDoInput = '';
+    ngOnInit(): void {
+        this.filteredToDos = this.toDos;
     }
-    this.filterToDos();
-  }
 
-  removeTask(task: string): void {
-    const index: number = this.toDos.findIndex(element => element.task === task);
-    this.toDos.splice(index, 1);
-    this.filterToDos();
-  }
+    addTask(): void {
+        if (this.toDoInput !== '') {
+            this.toDos.push({ task: this.toDoInput, completed: false });
+            this.toDoInput = '';
+        }
+        this.filterToDos();
+    }
 
-  completeTask(task: string): void {
-    const index: number = this.toDos.findIndex(element => element.task === task);
-    this.toDos[index].completed = true;
-    this.filterToDos();
-  }
-  
-  filterToDos(): void {
-    this.filteredToDos = this.toDos.filter(toDo => toDo.task.includes(this.filterInput));
-  }
+    removeTask(task: string): void {
+        const index: number = this.toDos.findIndex(element => element.task === task);
+        this.toDos.splice(index, 1);
+        this.filterToDos();
+    }
+
+    completeTask(task: string): void {
+        const index: number = this.toDos.findIndex(element => element.task === task);
+        this.toDos[index].completed = true;
+        this.filterToDos();
+    }
+
+    filterToDos(): void {
+        this.filteredToDos = this.toDos.filter(toDo => toDo.task.includes(this.filterInput));
+    }
 
 }
